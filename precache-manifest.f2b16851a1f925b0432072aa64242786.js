6fddaeedb66013a6211320428178bc6755e843ee
José Donaldo Fernández Montenegro <donaldo@MacBook-Pro-de-Jose.local>
2020-03-16 22:06:06 -0600

    Updates

---
+++ precache-manifest.f2b16851a1f925b0432072aa64242786.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8db41f200a72ea7742b7bce44a1ced",
+    "url": "/curriculumReact/index.html"
+  },
+  {
+    "revision": "78140bf7d1ac15def06f",
+    "url": "/curriculumReact/static/css/main.ee41b223.chunk.css"
+  },
+  {
+    "revision": "843da5fbbd3eb3a71649",
+    "url": "/curriculumReact/static/js/2.4eb5c35b.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/curriculumReact/static/js/2.4eb5c35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78140bf7d1ac15def06f",
+    "url": "/curriculumReact/static/js/main.d645d719.chunk.js"
+  },
+  {
+    "revision": "bc32341f80281927ace2",
+    "url": "/curriculumReact/static/js/runtime-main.39ba9ccf.js"
+  }
+]);
